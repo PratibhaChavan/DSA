69860e09e49d40087e97f8bc1c05cb1f74cb4667
PratibhaChavan <92856684+PratibhaChavan@users.noreply.github.com>
2023-02-16 22:59:59 +0530

    Update FindMinAndMaxValue.java

--- Array/Easy/FindMinAndMaxValue.java
+++ Array/Easy/FindMinAndMaxValue.java
@@ -1,13 +1,13 @@
-Maximum and minimum of an array using minimum number of comparisons
-Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
+// Maximum and minimum of an array using minimum number of comparisons
+// Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
 
-Examples:
+// Examples:
 
-Input: arr[] = {3, 5, 4, 1, 9}
-Output: Minimum element is: 1
-              Maximum element is: 9
+// Input: arr[] = {3, 5, 4, 1, 9}
+// Output: Minimum element is: 1
+//               Maximum element is: 9
            
-Asked in:ABCO Accolite Amazon Cisco Hike Microsoft Snapdeal VMWare Google Adobe
+// Asked in:ABCO Accolite Amazon Cisco Hike Microsoft Snapdeal VMWare Google Adobe
 
 public class FindMinAndMaxValue 
 {
