e0184d3c1a57fc6bd00189095e8c31ef281c6c03
PratibhaChavan <92856684+PratibhaChavan@users.noreply.github.com>
2023-02-16 23:03:03 +0530

    Update ReverseArray.java

--- Array/Easy/ReverseArray.java
+++ Array/Easy/ReverseArray.java
@@ -1,8 +1,20 @@
+// Write a program to reverse an array or string
+// Given an array (or string), the task is to reverse the array/string.
+// Examples : 
+ 
+
+// Input  : arr[] = {1, 2, 3}
+// Output : arr[] = {3, 2, 1}
+
+// Input :  arr[] = {4, 5, 1, 2}
+// Output : arr[] = {2, 1, 5, 4}
+// Asked in : Infosys Moonfrog Labs
+
 public class ReverseArray
 {
     public static void main(String args[])
     {
-        int arr[]={ 12,30,10,89,10,0};
+        int arr[]={1, 2, 3};
         for(int i=arr.length-1;i>=0;i--)
         {
             System.out.print(arr[i]+" ");
